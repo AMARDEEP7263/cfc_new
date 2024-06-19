386ab7c96e16f0f38806c486c7d035f0347e65e3
AMARDEEP7263 <amardeeptomar7263@gmail.com>
2024-06-19 23:18:30 +0530

    Boiler Plate Code - Basic Structure

---
+++ cfc-website/src/App.js
@@ -0,0 +1,17 @@
+// import logo from "./logo.svg";
+import "./App.css";
+// import SignInSide from "./LoginComponent/SignInSide";
+import LandingPage from "./LandingPage/LandingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        {/* <SignInSide /> */}
+        <LandingPage />
+      </>
+    </div>
+  );
+}
+
+export default App;
