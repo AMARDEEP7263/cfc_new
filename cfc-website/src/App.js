bdee43e8d228fa3dc5043a7f8f8290b3fcbf45d2
AMARDEEP7263 <amardeeptomar7263@gmail.com>
2024-06-22 09:49:19 +0530

    some change in handlechange function

--- cfc-website/src/App.js
+++ cfc-website/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <>
-        {!loginButton ? (
+        {loginButton ? (
           <LandingPage onSelect={handleClick} />
         ) : (
           <SignInSide onSelect={handleClick} />
